79d45c78e3f7f3902f7842a3aa6b19a9e66d16f6
Aryan-Diwakar-1856 <diwakerryan7900@gmail.com>
2024-07-05 00:52:56 +0530

    Login and register UI created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 
 import './App.css'
 import Login from './pages/login/Login'
+import Register from './pages/register/SignUp';
 
 function App() {
 
   return (
     <>
       <div className='p-4 h-screen flex items-center justify-center'>
-        <Login/>
+        {/* <Login/> */}
+        <Register/>
       </div>
     </>
   )
--- client/src/pages/register/SignUp.jsx
+++ client/src/pages/register/SignUp.jsx
@@ -2,7 +2,50 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+        <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+          <h1 className='text-3xl font-semibold text-center text-gray-300'>
+            Register to
+            <span className='text-blue-500'> BuzzTalk</span>
+          </h1> 
+          <form>
+            <div>
+              <label className='label p-2'>
+                <span className='text-base label-text'>Name</span>
+              </label>
+              <input type="text" placeholder='Ex. Hana Mathews' className='input input-bordered input-success w-full'/>
+            </div>
+
+            <div>
+              <label className='label p-2'>
+                <span className='text-base label-text'>Username</span>
+              </label>
+              <input type="text" placeholder='Ex. hanamathews' className='input input-bordered input-success w-full'/>
+            </div>
+
+            <div>
+              <label className='label p-2'>
+                <span className='text-base label-text'>Password</span>
+              </label>
+              <input type="password" placeholder='Enter Password' className='input input-bordered input-success w-full'/>
+            </div>
+
+            <div>
+              <label className='label p-2'>
+                <span className='text-base label-text'>Confirm Password</span>
+              </label>
+              <input type="text" placeholder='Confirm Password' className='input input-bordered input-success w-full'/>
+            </div>
+
+            <a href="#" className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block pt-2 pb-4 pl-2'>Already Have an Account?</a>
+
+            <div>
+              <button className='btn btn-outline btn-success w-full text-base'>Create Account</button>
+            </div>
+
+          </form>
+        </div>
+    </div>
   )
 }
 
